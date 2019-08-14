9ad25fc7f045063cc7f3a0284bdbd68fee5e218b
sorarideblog <54093651+sorarideblog@users.noreply.github.com>
2019-08-14 16:17:09 +0900

    Update game.js

--- game.js
+++ game.js
@@ -150,31 +150,6 @@ function recordLastAccess() {
   }
 }
 
-
-
-/*// ユーザー名を入力する
-function disp(){
-	// 入力ダイアログを表示 ＋ 入力内容を user に代入
-	user = window.prompt("ユーザー名を入力してください", "");
-	// 入力内容が tama の場合は example_tama.html にジャンプ
-	if(user == 'tama'){
-		location.href = "example_tama.html";
-	}
-	// 入力内容が hana の場合は example_hana.html にジャンプ
-	else if(user == 'hana'){
-		location.href = "example_hana.html";
-	}
-	// 入力内容が一致しない場合は警告ダイアログを表示
-	else if(user != "" && user != null){
-		window.alert(user + 'さんは登録されていません');
-	}
-	// 空の場合やキャンセルした場合は警告ダイアログを表示
-	else{
-		window.alert('キャンセルされました');
-	}
-}
-*/
-
 // ４つの回答ボタンに選択肢となる曲名を表示
 function writeText(i, str){
   switch(i){
