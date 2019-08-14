5004e60e05ae23a34b67f6f5992f740cfb5cd9b1
sorarideblog <54093651+sorarideblog@users.noreply.github.com>
2019-08-14 16:16:13 +0900

    Update init-firebase.js

--- init-firebase.js
+++ init-firebase.js
@@ -1,11 +1,11 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyBrt2hu98v7Ye8OJtM8-SD3NjMxZSD4MWU",
-  authDomain: "imas-intro-game.firebaseapp.com",
-  databaseURL: "https://intro-game.firebaseio.com",
-  projectId: "intro-game",
-  storageBucket: "intro-game.appspot.com",
+  apiKey: "APIキー",
+  authDomain: "プロジェクトID.firebaseapp.com",
+  databaseURL: "https://プロジェクトID.firebaseio.com",
+  projectId: "プロジェクトID",
+  storageBucket: "プロジェクトID.appspot.com",
   messagingSenderId: "sender-id",
-  appID: "1:833570921710:web:27cb8788235fb13b",
+  appID: "アプリID",
 };
 
 // Initialize Firebase
